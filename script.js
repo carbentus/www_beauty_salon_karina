58831455f597a8ff62e45b38a6dbb756175ba711
Karina Dalileh <karinad89@gmail.com>
2020-11-12 03:34:38 +0100

    toggle menu for mobile. Adjustment of header

--- script.js
+++ script.js
@@ -152,25 +152,34 @@ const navSlide = () => {
 
   burger.addEventListener("click", () => {
   // Toggle Nav
-    nav.classList.toggle("nav-active");
+  nav.classList.toggle("nav-active");
 
-  // Animate Links   (indeks = quantity of Links) , da opóźnienie wejścia dla każdego 
-    navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-                link.style.animation = '';
-        } else{
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.3}s`;
-        }
-  });
+  // close Nav after clicking anchor
+  navLinks.forEach((link) => {
+  
+    link.addEventListener("click", function () {
+      console.log("działa listener!!");
+      nav.classList.toggle("nav-active");
+      burger.classList.toggle('active');
+    });
+    });
 
-    // Burger toggle
-  burger.classList.toggle('active');
+  // Animate Links   (indeks = quantity of Links) , da opóźnienie wejścia dla każdego  
+    navLinks.forEach((link, index) => { 
+            if (link.style.animation) {
+                    link.style.animation = '';
+            } else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.3}s`;
+            }
+    
+      });
 
-// Listener koniec
+
+  // Burger toggle
+    burger.classList.toggle('active');
+
+// Listener end
   });
 }
 navSlide();
 
-
-
-//
