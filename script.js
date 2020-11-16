74b1867174c83f4a7982ee68cee35ba31ae5dd4f
Karina Dalileh <karinad89@gmail.com>
2020-11-16 12:13:01 +0100

    JS: shorter toggle menu animation

--- script.js
+++ script.js
@@ -182,9 +182,7 @@ const navSlide = () => {
       if (link.style.animation) {
         link.style.animation = "";
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 5 + 0.3
-        }s`;
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 9 + 0.1}s`;
       }
     });
 
