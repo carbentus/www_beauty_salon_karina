d0281edfe1988c554af3c696a73c777881092230
Karina Dalileh <karinad89@gmail.com>
2020-11-26 14:17:30 +0100

    Connect emailform to Formspree

--- script.js
+++ script.js
@@ -190,8 +190,8 @@ window.addEventListener("resize", () => {
 
 // RECAPTCHA alert (when the recaptcha is not checked)
 
-var form = document.getElementById('form');
-form.addEventListener("submit", function (event) {
+var form = document.getElementById('my-form');
+form.addEventListener('submit', function (event) {
   if (grecaptcha.getResponse() === '') {
     event.preventDefault();
     alert('Please check the recaptcha');
@@ -200,5 +200,55 @@ form.addEventListener("submit", function (event) {
   , false);
 
 var onloadCallback = function () {
-//   alert("grecaptcha is ready!");
+  //   alert("grecaptcha is ready!");
 };
+
+
+// ****** EMAIL ****** FORMSPREE
+
+window.addEventListener("DOMContentLoaded", function () {
+
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("my-form");
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById("form-status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Bedankt, uw bericht is met succes verzonden!";
+  }
+
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "Ooops! Er is iets misgegaan. Probeer het bericht opnieuw te versturen of neem telefonisch contact met ons op.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
